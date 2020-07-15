f7cd4950f94a89bb0b2f0ea43211d15999a1a67c
Hunter Bennett <hunter.ben28@hotmail.com>
2020-07-15 11:26:26 -0400

    Testing same window panel switching

--- src/SakilaHome.java
+++ src/SakilaHome.java
@@ -11,18 +11,75 @@ import javax.swing.*;
 
 public class SakilaHome extends JFrame
 {
-	final int WINDOW_WIDTH = 400;
-	final int WINDOW_HEIGHT = 600;
-	
+	final int WINDOW_WIDTH = 600;
+	final int WINDOW_HEIGHT = 400;
+	BorderLayout homePage;
+	public JPanel homePanel;
+	public JPanel otherPanel;
  public SakilaHome()
  {
+	 super("Sakila Database");
 	 this.setLocationRelativeTo(null);
 	 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	 this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 	 
 	 //Decide layout
-	 //this.setLayout(new ...);
+	 this.setLayout(new FlowLayout());
+	 
+	 homePage = new BorderLayout();
+	 homePanel = new JPanel(homePage);
+	 JButton homeBtn = new JButton("Home Panel");
+	 homeBtn.addActionListener(new MyListener(homePanel));
+	 homePanel.add(homeBtn);
+	 
+	 otherPanel = new JPanel(new FlowLayout());
+	 JButton otherButton = new JButton("Page 2");
+	 otherPanel.add(otherButton);
+	 otherPanel.add(new JLabel("This is a label"));
+	 otherButton.addActionListener(new MyListener(otherPanel));
+
+	 
+	 this.add(homePanel);
+	 
 	 
 	 this.setVisible(true);
+ }
+ 
+ public JPanel getPanel()
+ {
+	 return homePanel;
+ }
+ 
+ public void setPanel(JPanel panel)
+ {
+	 this.homePanel = panel;
+ }
+ 
+private class MyListener implements ActionListener
+{
+	 JPanel panel;
+	 
+	 public MyListener(JPanel panel)
+	 {
+		 this.panel = panel;
+	 }
+	 
+	 public void actionPerformed(ActionEvent ev)
+	 {
+		 changePanel(panel);
+	 }
+}
+
+public void changePanel(JPanel panel)
+{
+	this.getContentPane().removeAll();
+	this.getContentPane().add(panel);
+	this.getContentPane().doLayout();
+	this.update(getGraphics());
+}
+ 
+public static void main(String[] args)
+ {
+	 new SakilaHome();
  }
 }
